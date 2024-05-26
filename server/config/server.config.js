23bea9cb62b75f93302bdb471aa63e1829725b5c
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-05-27 02:03:52 +0530

    refactor: Remove unused CLIENT_URL variable from server config

--- server/config/server.config.js
+++ server/config/server.config.js
@@ -4,11 +4,9 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 const NODE_ENV = process.env.NODE_ENV;
 const JWT_SECRET = process.env.JWT_SECRET;
-const CLIENT_URL = process.env.CLIENT_URL;
 const SESSION_SECRET = process.env.SESSION_SECRET;
 
 module.exports = {
-  CLIENT_URL,
   JWT_SECRET,
   MONGODB_URI,
   NODE_ENV,
