fc439c14759fb2837e716e879b187e6933f8913e
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-05-27 01:52:52 +0530

    chore: Update sameSite attribute to "none" for JWT cookie

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: NODE_ENV === "production" ? true : false,
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
