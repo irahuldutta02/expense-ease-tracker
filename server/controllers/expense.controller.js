c66e9b826c035b02bff9dfb9df412c0fb031313e
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-05-27 19:12:06 +0530

    feat

--- server/controllers/expense.controller.js
+++ server/controllers/expense.controller.js
@@ -5,19 +5,38 @@ const allListByUserId = asyncHandler(async (req, res) => {
   try {
     const UserId = req.user._id;
 
-    const expenses = await Expense.find({ UserId: UserId })
+    const expenses = await Expense.find({ UserId })
       .populate("Mode")
       .populate("Category")
       .populate("Party")
-      .populate("UserId", "-password");
+      .populate("UserId", "-password")
+      .sort({ createdAt: "asc" });
 
-    expenses.sort((a, b) => {
-      return new Date(b.Date) - new Date(a.Date);
-    });
+    // Sort expenses by date in ascending order
+    const sortedExpenses = expenses.sort(
+      (a, b) => new Date(a.Date) - new Date(b.Date)
+    );
+
+    // Add the balance key to each expense record
+    const result = sortedExpenses.map((expense) => expense.toObject()); // Convert all expenses to plain objects
+    for (let i = 0; i < result.length; i++) {
+      if (i === 0) {
+        result[i].balance = result[i].Cash_In
+          ? result[i].Cash_In
+          : -result[i].Cash_Out;
+      } else {
+        result[i].balance =
+          result[i - 1].balance +
+          (result[i].Cash_In ? result[i].Cash_In : -result[i].Cash_Out);
+      }
+    }
+
+    // reverse the array
+    result.reverse();
 
     return res.status(200).json({
       status: 200,
-      data: expenses,
+      data: result,
     });
   } catch (error) {
     res.status(404);
