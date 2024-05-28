5b3f482663319dc33162f7464281158eb36c1e95
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-05-28 11:36:30 +0530

    feat: Add CRUD operations for category API

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -20,4 +20,100 @@ const allListByUserId = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { allListByUserId };
+const createCategory = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const categoryExist = await Category.findOne({
+      Name: name,
+      UserId: req?.user?._id,
+    });
+
+    if (categoryExist) {
+      res.status(400);
+      throw new Error("Category already exists");
+    }
+
+    const category = new Category({
+      Name: name,
+      UserId: req?.user?._id,
+    });
+
+    const createdCategory = await category.save();
+
+    return res.status(201).json({
+      status: 201,
+      data: createdCategory,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404);
+    throw new Error(error?.message || "Category not created");
+  }
+});
+
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const UserId = req?.user?._id;
+
+    const { name } = req.body;
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+      res.status(404);
+      throw new Error("Category not found");
+    }
+
+    if (category?.UserId.toString() !== UserId.toString()) {
+      res.status(401);
+      throw new Error("Not authorized to update this category");
+    }
+
+    category.Name = name;
+
+    const updatedCategory = await category.save();
+
+    return res.status(200).json({
+      status: 200,
+      data: updatedCategory,
+    });
+  } catch (error) {
+    res.status(404);
+    throw new Error(error?.message || "Category not updated");
+  }
+});
+
+const deleteCategory = asyncHandler(async (req, res) => {
+  try {
+    const UserId = req?.user?._id;
+
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+      res.status(404);
+      throw new Error("Category not found");
+    }
+
+    if (category?.UserId.toString() !== UserId.toString()) {
+      res.status(401);
+      throw new Error("Not authorized to delete this category");
+    }
+
+    await Category.findByIdAndDelete(req.params.id);
+
+    return res.status(200).json({
+      status: 200,
+      message: "Category removed",
+    });
+  } catch (error) {
+    res.status(404);
+    throw new Error(error?.message || "Category not deleted");
+  }
+});
+
+module.exports = {
+  allListByUserId,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+};
--- server/routes/category.routes.js
+++ server/routes/category.routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { allListByUserId } = require("../controllers/category.controller.js");
+const {
+  allListByUserId,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/category.controller.js");
 const router = express.Router();
 
 const { protect } = require("../middleware/auth.middleware.js");
 
 router.route("/").get(protect, allListByUserId);
+router.route("/").post(protect, createCategory);
+router.route("/:id").put(protect, updateCategory);
+router.route("/:id").delete(protect, deleteCategory);
 
 module.exports = router;
