6fb6add9cd6352cc2388bf56f35e4b2cc8fca8eb
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-05-31 08:53:36 +0530

    feat: Add create, update, and delete endpoints for modes API

--- server/controllers/mode.controller.js
+++ server/controllers/mode.controller.js
@@ -5,7 +5,10 @@ const allListByUserId = asyncHandler(async (req, res) => {
   try {
     const UserId = req.user._id;
 
-    const modes = await Mode.find({ UserId: UserId }).populate("UserId", "-password");
+    const modes = await Mode.find({ UserId: UserId }).populate(
+      "UserId",
+      "-password"
+    );
 
     return res.status(200).json({
       status: 200,
@@ -17,4 +20,95 @@ const allListByUserId = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { allListByUserId };
+const createMode = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const modeExists = await Mode.findOne({
+      Name: name,
+      UserId: req?.user?._id,
+    });
+
+    if (modeExists) {
+      res.status(400);
+      throw new Error("Mode already exists");
+    }
+
+    const mode = new Mode({
+      Name: name,
+      UserId: req?.user?._id,
+    });
+
+    const createdMode = await mode.save();
+
+    return res.status(201).json({
+      status: 201,
+      data: createdMode,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404);
+    throw new Error(error?.message || "Mode not created");
+  }
+});
+
+const updateMode = asyncHandler(async (req, res) => {
+  try {
+    const UserId = req?.user?._id;
+
+    const { name } = req.body;
+    const mode = await Mode.findById(req.params.id);
+
+    if (!mode) {
+      res.status(404);
+      throw new Error("Mode not found");
+    }
+
+    if (mode?.UserId.toString() !== UserId.toString()) {
+      res.status(401);
+      throw new Error("Not authorized to update this mode");
+    }
+
+    mode.Name = name;
+
+    const updatedMode = await mode.save();
+
+    return res.status(200).json({
+      status: 200,
+      data: updatedMode,
+    });
+  } catch (error) {
+    res.status(404);
+    throw new Error(error?.message || "mode not updated");
+  }
+});
+
+const deleteMode = asyncHandler(async (req, res) => {
+  try {
+    const UserId = req?.user?._id;
+
+    const mode = await Mode.findById(req.params.id);
+
+    if (!mode) {
+      res.status(404);
+      throw new Error("Mode not found");
+    }
+
+    if (mode?.UserId.toString() !== UserId.toString()) {
+      res.status(401);
+      throw new Error("Not authorized to delete this mode");
+    }
+
+    await Mode.findByIdAndDelete(req.params.id);
+
+    return res.status(200).json({
+      status: 200,
+      message: "Mode removed",
+    });
+  } catch (error) {
+    res.status(404);
+    throw new Error(error?.message || "Mode not deleted");
+  }
+});
+
+module.exports = { allListByUserId, createMode, updateMode, deleteMode };
--- server/routes/mode.routes.js
+++ server/routes/mode.routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { allListByUserId } = require("../controllers/mode.controller.js");
+const {
+  allListByUserId,
+  createMode,
+  updateMode,
+  deleteMode,
+} = require("../controllers/mode.controller.js");
 const router = express.Router();
 
 const { protect } = require("../middleware/auth.middleware.js");
 
 router.route("/").get(protect, allListByUserId);
+router.route("/").post(protect, createMode);
+router.route("/:id").put(protect, updateMode);
+router.route("/:id").delete(protect, deleteMode);
 
 module.exports = router;
