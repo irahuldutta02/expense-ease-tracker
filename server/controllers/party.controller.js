1d538cb7013941b9cefaa3792370663b7db653bd
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-05-31 09:01:13 +0530

    feat: Add create, update, and delete endpoints for party API

--- server/controllers/party.controller.js
+++ server/controllers/party.controller.js
@@ -20,4 +20,95 @@ const allListByUserId = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { allListByUserId };
+const createParty = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const partyExists = await Party.findOne({
+      Name: name,
+      UserId: req?.user?._id,
+    });
+
+    if (partyExists) {
+      res.status(400);
+      throw new Error("Party already exists");
+    }
+
+    const party = new Party({
+      Name: name,
+      UserId: req?.user?._id,
+    });
+
+    const createdParty = await party.save();
+
+    return res.status(201).json({
+      status: 201,
+      data: createdParty,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404);
+    throw new Error(error?.message || "Party not created");
+  }
+});
+
+const updateParty = asyncHandler(async (req, res) => {
+  try {
+    const UserId = req?.user?._id;
+
+    const { name } = req.body;
+    const party = await Party.findById(req.params.id);
+
+    if (!party) {
+      res.status(404);
+      throw new Error("Party not found");
+    }
+
+    if (party?.UserId.toString() !== UserId.toString()) {
+      res.status(401);
+      throw new Error("Not authorized to update this party");
+    }
+
+    party.Name = name;
+
+    const updatedParty = await party.save();
+
+    return res.status(200).json({
+      status: 200,
+      data: updatedParty,
+    });
+  } catch (error) {
+    res.status(404);
+    throw new Error(error?.message || "party not updated");
+  }
+});
+
+const deleteParty = asyncHandler(async (req, res) => {
+  try {
+    const UserId = req?.user?._id;
+
+    const party = await Party.findById(req.params.id);
+
+    if (!party) {
+      res.status(404);
+      throw new Error("Party not found");
+    }
+
+    if (party?.UserId.toString() !== UserId.toString()) {
+      res.status(401);
+      throw new Error("Not authorized to delete this party");
+    }
+
+    await Party.findByIdAndDelete(req.params.id);
+
+    return res.status(200).json({
+      status: 200,
+      message: "Party removed",
+    });
+  } catch (error) {
+    res.status(404);
+    throw new Error(error?.message || "Party not deleted");
+  }
+});
+
+module.exports = { allListByUserId, createParty, updateParty, deleteParty };
--- server/routes/party.routes.js
+++ server/routes/party.routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { allListByUserId } = require("../controllers/party.controller.js");
+const {
+  allListByUserId,
+  createParty,
+  updateParty,
+  deleteParty,
+} = require("../controllers/party.controller.js");
 const router = express.Router();
 
 const { protect } = require("../middleware/auth.middleware.js");
 
 router.route("/").get(protect, allListByUserId);
+router.route("/").post(protect, createParty);
+router.route("/:id").put(protect, updateParty);
+router.route("/:id").delete(protect, deleteParty);
 
 module.exports = router;
