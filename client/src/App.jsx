d6fbf95e247139248b4d52a56cb5ac36b1134260
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-06-02 14:06:49 +0530

    feat: Update PWA assets and configuration for Vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { Toaster } from "react-hot-toast";
-import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import { MainRoutes } from "./Routes/MainRoutes";
 import { ThemeContext } from "./context/ContextProvider";
