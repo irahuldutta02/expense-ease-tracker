8e84629418ac462a10b570d00577a46a9d3b185c
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-07-27 14:48:32 +0530

    refactor: Update token expiration to 1 day in generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -3,7 +3,7 @@ const { JWT_SECRET } = require("../config/server.config.js");
 
 const generateToken = (userId) => {
   const token = jwt.sign({ userId }, JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "1d",
   });
 
   return token;
